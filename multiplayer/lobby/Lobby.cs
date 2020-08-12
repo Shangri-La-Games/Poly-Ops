f0af2e5ea46da2ad0860741bf05178051507f159
Sanjay Gubaju <8557287+SanjayGubaju@users.noreply.github.com>
2020-08-12 15:31:07 +0545

    updated lobby

--- multiplayer/lobby/Lobby.cs
+++ multiplayer/lobby/Lobby.cs
@@ -3,140 +3,140 @@ using System;
 
 public class Lobby : Control
 {
-    Panel connectPanel;
-    Panel playersPanel;
-    ItemList playersList;
-    LineEdit nameEdit;
-    LineEdit ipEdit;
-    Button hostButton;
-    Button joinButton;
-    Button startButton;
-    AcceptDialog dialog;
+	Panel connectPanel;
+	Panel playersPanel;
+	ItemList playersList;
+	LineEdit nameEdit;
+	LineEdit ipEdit;
+	Button hostButton;
+	Button joinButton;
+	Button startButton;
+	AcceptDialog dialog;
 
-    public override void _Ready()
-    {
-        // Initialize global events
-        GameState.instance.Connect("connectionFailed", this, "connectionFailed");
-        GameState.instance.Connect("connectionSuccessful", this, "connectionSuccessful");
-        GameState.instance.Connect("playerUpdated", this, "refreshLobby");
-        GameState.instance.Connect("gameEnded", this, "gameEnded");
-        GameState.instance.Connect("gameError", this, "gameError");
+	public override void _Ready()
+	{
+		// Initialize global events
+		GameState.instance.Connect("connectionFailed", this, "connectionFailed");
+		GameState.instance.Connect("connectionSuccessful", this, "connectionSuccessful");
+		GameState.instance.Connect("playerUpdated", this, "refreshLobby");
+		GameState.instance.Connect("gameEnded", this, "gameEnded");
+		GameState.instance.Connect("gameError", this, "gameError");
 
-        // Initialize UI events
-        connectPanel = (Panel)GetNode("connect");
-        playersPanel = (Panel)GetNode("players");
-        dialog = (AcceptDialog)GetNode("dialog");
+		// Initialize UI events
+		connectPanel = (Panel)GetNode("connect");
+		playersPanel = (Panel)GetNode("players");
+		dialog = (AcceptDialog)GetNode("dialog");
 
-        // Initialize visibility
-        connectPanel.Hide();
-        playersPanel.Show();
+		// Initialize visibility
+		connectPanel.Hide();
+		playersPanel.Show();
 
-        // -- Name, IP Input --
-        nameEdit = (LineEdit)playersPanel.GetNode("v_container/edit_name");
-        ipEdit = (LineEdit)playersPanel.GetNode("v_container/edit_ip");
+		// -- Name, IP Input --
+		nameEdit = (LineEdit)playersPanel.GetNode("v_container/edit_name");
+		ipEdit = (LineEdit)playersPanel.GetNode("v_container/edit_ip");
 
-        // Players list
-        playersList = (ItemList)connectPanel.GetNode("v_container/list_player");
+		// Players list
+		playersList = (ItemList)connectPanel.GetNode("v_container/list_player");
 
-        // -- Host --
-        hostButton = (Button)playersPanel.GetNode("v_container/h_container/btn_host");
-        hostButton.Connect("pressed", this, "handleHost");
+		// -- Host --
+		hostButton = (Button)playersPanel.GetNode("v_container/h_container/btn_host");
+		hostButton.Connect("pressed", this, "handleHost");
 
-        // -- Join --
-        joinButton = (Button)playersPanel.GetNode("v_container/h_container/btn_join");
-        joinButton.Connect("pressed", this, "handleJoin");
+		// -- Join --
+		joinButton = (Button)playersPanel.GetNode("v_container/h_container/btn_join");
+		joinButton.Connect("pressed", this, "handleJoin");
 
-        // -- Start --
-        startButton = (Button)connectPanel.GetNode("v_container/btn_start");
-        startButton.Connect("pressed", this, "handleStart");
-    }
+		// -- Start --
+		startButton = (Button)connectPanel.GetNode("v_container/btn_start");
+		startButton.Connect("pressed", this, "handleStart");
+	}
 
-    private void handleJoin()
-    {
-        String name = nameEdit.Text;
-        if (name == "")
-        {
-            dialog.DialogText = "Name is invalid";
-            return;
-        }
+	private void handleJoin()
+	{
+		String name = nameEdit.Text;
+		if (name == "")
+		{
+			dialog.DialogText = "Name is invalid";
+			return;
+		}
 
-        String ipAddress = ipEdit.Text;
-        if (!ipAddress.IsValidIPAddress())
-        {
-            dialog.DialogText = "Invalid IP address";
-            return;
-        }
+		String ipAddress = ipEdit.Text;
+		if (!ipAddress.IsValidIPAddress())
+		{
+			dialog.DialogText = "Invalid IP address";
+			return;
+		}
 
-        hostButton.Disabled = true;
-        joinButton.Disabled = true;
-        dialog.DialogText = "";
+		hostButton.Disabled = true;
+		joinButton.Disabled = true;
+		dialog.DialogText = "";
 
-        // Join game
-        GameState.Instance.joinGame(ipAddress, name);
-        refreshLobby();
-    }
+		// Join game
+		GameState.Instance.joinGame(ipAddress, name);
+		refreshLobby();
+	}
 
-    private void handleHost()
-    {
-        String name = nameEdit.Text;
-        if (name == "")
-        {
-            dialog.DialogText = "Name is invalid";
-            return;
-        }
+	private void handleHost()
+	{
+		String name = nameEdit.Text;
+		if (name == "")
+		{
+			dialog.DialogText = "Name is invalid";
+			return;
+		}
 
-        // Clear UI
-        connectPanel.Show();
-        playersPanel.Hide();
-        dialog.DialogText = "";
+		// Clear UI
+		connectPanel.Show();
+		playersPanel.Hide();
+		dialog.DialogText = "";
 
-        // Host game
-        GameState.Instance.hostGame(nameEdit.Text);
-        refreshLobby();
-    }
+		// Host game
+		GameState.Instance.hostGame(nameEdit.Text);
+		refreshLobby();
+	}
 
-    private void handleStart() { GameState.Instance.startGame(); }
+	private void handleStart() { GameState.Instance.startGame(); }
 
-    private void connectionSuccessful()
-    {
-        connectPanel.Show();
-        playersPanel.Hide();
-    }
+	private void connectionSuccessful()
+	{
+		connectPanel.Show();
+		playersPanel.Hide();
+	}
 
-    private void connectionFailed()
-    {
-        hostButton.Disabled = false;
-        joinButton.Disabled = false;
-        dialog.DialogText = "Connection failed";
-    }
+	private void connectionFailed()
+	{
+		hostButton.Disabled = false;
+		joinButton.Disabled = false;
+		dialog.DialogText = "Connection failed";
+	}
 
-    private void gameEnded()
-    {
-        // Hide();
-        connectPanel.Hide();
-        playersPanel.Show();
+	private void gameEnded()
+	{
+		// Hide();
+		connectPanel.Hide();
+		playersPanel.Show();
 
-        hostButton.Disabled = false;
-        joinButton.Disabled = false;
-    }
+		hostButton.Disabled = false;
+		joinButton.Disabled = false;
+	}
 
-    private void gameError(String error)
-    {
-        dialog.DialogText = error;
-    }
+	private void gameError(String error)
+	{
+		dialog.DialogText = error;
+	}
 
-    private void refreshLobby()
-    {
-        playersList.Clear();
+	private void refreshLobby()
+	{
+		playersList.Clear();
 
-        // Add current player.
-        playersList.AddItem(String.Format("{0} (You)", GameState.Instance.getPlayerName()));
+		// Add current player.
+		playersList.AddItem(String.Format("{0} (You)", GameState.Instance.getPlayerName()));
 
-        // Add other players
-        foreach (String p in GameState.Instance.getPlayerList()) { playersList.AddItem(p); }
+		// Add other players
+		foreach (String p in GameState.Instance.getPlayerList()) { playersList.AddItem(p); }
 
-        // Enable/Disable start button
-        startButton.Disabled = !GetTree().IsNetworkServer();
-    }
+		// Enable/Disable start button
+		startButton.Disabled = !GetTree().IsNetworkServer();
+	}
 
 }
--- multiplayer/prefabs/puppet/PuppetController.cs
+++ multiplayer/prefabs/puppet/PuppetController.cs
@@ -4,59 +4,59 @@ using System;
 // Represents oneself
 public class PuppetController : CharacterController
 {
-	private bool captured = true;
-	private float sensibility = 0.2f;
-	public void captureMouse() { this.captured = !captured; }
+    private bool captured = true;
+    private float sensibility = 0.2f;
+    public void captureMouse() { this.captured = !captured; }
 
-	public override void _PhysicsProcess(float delta) { handleInput(); toggleMouse(); }
-	public override void _Input(InputEvent @event) { handleMouse(@event); }
+    public override void _PhysicsProcess(float delta) { handleInput(); toggleMouse(); }
+    public override void _Input(InputEvent @event) { handleMouse(@event); }
 
-	private void handleInput()
-	{
-		// Do not allow input if mouse is inactive.
-		if (!captured) { return; }
+    private void handleInput()
+    {
+        // Do not allow input if mouse is inactive.
+        if (!captured) { return; }
 
-		this.player.input["left"] = Convert.ToInt32(Input.IsActionPressed("KEY_A"));
-		this.player.input["right"] = Convert.ToInt32(Input.IsActionPressed("KEY_D"));
-		this.player.input["forward"] = Convert.ToInt32(Input.IsActionPressed("KEY_W"));
-		this.player.input["back"] = Convert.ToInt32(Input.IsActionPressed("KEY_S"));
-		this.player.input["switch"] = Convert.ToInt32(Input.IsActionJustPressed("KEY_E"));
-		this.player.input["shoot"] = Convert.ToInt32(Input.IsActionPressed("MOUSE_LEFT"));
-		this.player.input["reload"] = Convert.ToInt32(Input.IsActionPressed("KEY_R"));
-		this.player.input["focus"] = Convert.ToInt32(Input.IsActionPressed("MOUSE_RIGHT"));
-		this.player.input["crouch"] = Convert.ToInt32(Input.IsActionPressed("KEY_CTRL"));
-		this.player.input["jump"] = Convert.ToInt32(Input.IsActionPressed("KEY_SPACE"));
-		this.player.input["sprint"] = Convert.ToInt32(Input.IsActionPressed("KEY_SHIFT"));
-	}
+        this.player.input["left"] = Convert.ToInt32(Input.IsActionPressed("KEY_A"));
+        this.player.input["right"] = Convert.ToInt32(Input.IsActionPressed("KEY_D"));
+        this.player.input["forward"] = Convert.ToInt32(Input.IsActionPressed("KEY_W"));
+        this.player.input["back"] = Convert.ToInt32(Input.IsActionPressed("KEY_S"));
+        this.player.input["switch"] = Convert.ToInt32(Input.IsActionJustPressed("KEY_E"));
+        this.player.input["shoot"] = Convert.ToInt32(Input.IsActionPressed("MOUSE_LEFT"));
+        this.player.input["reload"] = Convert.ToInt32(Input.IsActionPressed("KEY_R"));
+        this.player.input["focus"] = Convert.ToInt32(Input.IsActionPressed("MOUSE_RIGHT"));
+        this.player.input["crouch"] = Convert.ToInt32(Input.IsActionPressed("KEY_CTRL"));
+        this.player.input["jump"] = Convert.ToInt32(Input.IsActionPressed("KEY_SPACE"));
+        this.player.input["sprint"] = Convert.ToInt32(Input.IsActionPressed("KEY_SHIFT"));
+    }
 
-	// Toggle mouse capture
-	private void toggleMouse()
-	{
-		if (Input.IsActionJustPressed("KEY_ESCAPE")) { captured = !captured; }
-		if (captured) { Input.SetMouseMode(Input.MouseMode.Captured); this.player.resume(); }
-		else { Input.SetMouseMode(Input.MouseMode.Visible); this.player.pause(); }
-	}
+    // Toggle mouse capture
+    private void toggleMouse()
+    {
+        if (Input.IsActionJustPressed("KEY_ESCAPE")) { captured = !captured; }
+        if (captured) { Input.SetMouseMode(Input.MouseMode.Captured); this.player.resume(); }
+        else { Input.SetMouseMode(Input.MouseMode.Visible); this.player.pause(); }
+    }
 
-	private void handleMouse(InputEvent @event)
-	{
-		if (captured)
-		{
-			if (@event is InputEventMouseMotion)
-			{
-				InputEventMouseMotion input = @event as InputEventMouseMotion;
+    private void handleMouse(InputEvent @event)
+    {
+        if (captured)
+        {
+            if (@event is InputEventMouseMotion)
+            {
+                InputEventMouseMotion input = @event as InputEventMouseMotion;
 
-				float newRotationX = player.headNode.Rotation.x + -Mathf.Deg2Rad(input.Relative.y * sensibility);
-				player.headNode.Rotation = new Vector3(newRotationX, player.headNode.Rotation.y, player.headNode.Rotation.z);
+                float newRotationX = player.headNode.Rotation.x + -Mathf.Deg2Rad(input.Relative.y * sensibility);
+                player.headNode.Rotation = new Vector3(newRotationX, player.headNode.Rotation.y, player.headNode.Rotation.z);
 
-				float newRotationY = player.Rotation.y + -Mathf.Deg2Rad(input.Relative.x * sensibility);
-				player.Rotation = new Vector3(player.Rotation.x, newRotationY, player.Rotation.z);
-			}
+                float newRotationY = player.Rotation.y + -Mathf.Deg2Rad(input.Relative.x * sensibility);
+                player.Rotation = new Vector3(player.Rotation.x, newRotationY, player.Rotation.z);
+            }
 
-			// Create a limit for camera on x-axis
-			Int32 maxAngle = 60;
-			player.headNode.Rotation = new Vector3(Math.Min(player.headNode.Rotation.x, Mathf.Deg2Rad(maxAngle)), player.headNode.Rotation.y, player.headNode.Rotation.z);
-			player.headNode.Rotation = new Vector3(Math.Max(player.headNode.Rotation.x, -Mathf.Deg2Rad(maxAngle)), player.headNode.Rotation.y, player.headNode.Rotation.z);
-		}
-	}
+            // Create a limit for camera on x-axis
+            Int32 maxAngle = 60;
+            player.headNode.Rotation = new Vector3(Math.Min(player.headNode.Rotation.x, Mathf.Deg2Rad(maxAngle)), player.headNode.Rotation.y, player.headNode.Rotation.z);
+            player.headNode.Rotation = new Vector3(Math.Max(player.headNode.Rotation.x, -Mathf.Deg2Rad(maxAngle)), player.headNode.Rotation.y, player.headNode.Rotation.z);
+        }
+    }
 
 }
